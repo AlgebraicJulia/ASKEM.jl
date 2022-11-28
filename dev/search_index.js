54b855578131a08df5d240869828ba6ecf442f7c
Documenter.jl <documenter@juliadocs.github.io>
2022-11-28 19:20:58 +0000

    build based on 2ac0d34

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ASKEM","category":"page"},{"location":"#ASKEM","page":"Home","title":"ASKEM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ASKEM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ASKEM]","category":"page"}]
+}
