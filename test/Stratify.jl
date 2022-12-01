5a39ceae3e4fae981ec7fb38fbd48bf7b54d6ee0
Owen Lynch <root@owenlynch.org>
2022-12-01 15:52:52 -0800

    TEST: fixed tests

--- test/Stratify.jl
+++ test/Stratify.jl
@@ -14,21 +14,22 @@ Quarantine = LabelledPetriNet([:Q,:NQ],
     :quarantine => ((:NQ)=>(:Q)),
     :unquarantine => ((:Q)=>(:NQ)))
 
-SIRD_typed = homomorphism(SIRD, infectious_ontology;
+SIRD_typed = homomorphism(SIRD, strip_names(infectious_ontology);
     initial=(T=[1,2,2],I=[1,2,3,3],O=[1,2,3,3]),
     type_components=(Name=x->nothing,))
 
-Quarantine_typed = homomorphism(Quarantine, infectious_ontology;
+Quarantine_typed = homomorphism(Quarantine, strip_names(infectious_ontology);
     initial=(T=[3,3],), type_components=(Name=x->nothing,))
 
 naive_stratification = apex(pullback(SIRD_typed, Quarantine_typed))
 @test nt(naive_stratification) == 0 
 
 res = stratify(SIRD_typed=>[[:strata],[:strata],[:strata],[]], # S I R D
-                Quarantine_typed=>[[:disease], [:disease,:infect]], # Q NQ
-                types′) 
+               Quarantine_typed=>[[:disease], [:disease,:infect]],# Q NQ
+               infectious_ontology)
+
 res2 = stratify_except(SIRD_typed=>[4=>[:strata]], 
-                        Quarantine_typed=>[1=>[:infect]], types′)
+                       Quarantine_typed=>[1=>[:infect]], infectious_ontology)
 
 @test is_isomorphic(res, res2)
 
