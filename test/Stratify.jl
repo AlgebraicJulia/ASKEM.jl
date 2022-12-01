30743bdbc9b2a8de6f2f8f791daa9cee7f842b84
Owen Lynch <root@owenlynch.org>
2022-12-01 15:16:04 -0800

    TEST: use the premade ontology in the stratification tests

--- test/Stratify.jl
+++ test/Stratify.jl
@@ -14,25 +14,18 @@ Quarantine = LabelledPetriNet([:Q,:NQ],
     :quarantine => ((:NQ)=>(:Q)),
     :unquarantine => ((:Q)=>(:NQ)))
 
-types′ = LabelledPetriNet([:Pop],
-    :infect=>((:Pop, :Pop)=>(:Pop, :Pop)),
-    :disease=>(:Pop=>:Pop),
-    :strata=>(:Pop=>:Pop))
-
-types = map(types′, Name=name->nothing)
-
-SIRD_typed = homomorphism(SIRD, types;
+SIRD_typed = homomorphism(SIRD, infectious_ontology;
     initial=(T=[1,2,2],I=[1,2,3,3],O=[1,2,3,3]),
     type_components=(Name=x->nothing,))
 
-Quarantine_typed = homomorphism(Quarantine, types;
+Quarantine_typed = homomorphism(Quarantine, infectious_ontology;
     initial=(T=[3,3],), type_components=(Name=x->nothing,))
 
 naive_stratification = apex(pullback(SIRD_typed, Quarantine_typed))
 @test nt(naive_stratification) == 0 
 
 res = stratify(SIRD_typed=>[[:strata],[:strata],[:strata],[]], # S I R D
-                Quarantine_typed=>[[:disease], [:disease,:infect]],# Q NQ 
+                Quarantine_typed=>[[:disease], [:disease,:infect]], # Q NQ
                 types′) 
 res2 = stratify_except(SIRD_typed=>[4=>[:strata]], 
                         Quarantine_typed=>[1=>[:infect]], types′)
