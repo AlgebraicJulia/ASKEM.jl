b569f3a77c267cd01683a0b8f01a65f32d206cae
Owen Lynch <root@owenlynch.org>
2022-12-04 10:10:17 -0800

    added intervention simulation code

--- src/ASKEM.jl
+++ src/ASKEM.jl
@@ -3,11 +3,13 @@ module ASKEM
 using Reexport
 
 include("PetriMTK.jl")
+include("Interventions.jl")
 include("Interactions.jl")
 include("Stratify.jl")
 include("Ontologies.jl")
 
 @reexport using .PetriMTK
+@reexport using .Interventions
 @reexport using .Interactions
 @reexport using .Stratify
 @reexport using .Ontologies
--- src/Interventions.jl
+++ src/Interventions.jl
@@ -1,30 +1,25 @@
 module Interventions
+export caseload_triggered_lockdown, time_triggered_lockdown
 
 using ModelingToolkit
 
-abstract type Trigger end
-
-# Apply the equations at a given time
-struct TimedTrigger <: Trigger
-  time::Float64
+function caseload_triggered_lockdown(model, cases_expression, threshold, lockdown_effect)
+  ODESystem(
+    [],
+    ModelingToolkit.get_iv(model);
+    continuous_events = [cases_expression ~ threshold] => [model.inf ~ lockdown_effect * model.inf],
+    name = :caseload_triggered_lockdown
+  )
 end
 
-# Threshold intervention
-struct ThresholdTrigger <: Trigger
-  variable::Symbolic
-  threshold::Float64
-end
-
-abstract type Effect end
-
-struct SetEffect <: Effect
-  variable::Symbolic
-  value::Float64
-end
-
-struct Policy
-  trigger::Trigger
-  effect::Effect
+function time_triggered_lockdown(model, T, lockdown_effect)
+  t = ModelingToolkit.get_iv(model)
+  ODESystem(
+    [],
+    t;
+    discrete_events = [T] => [model.inf ~ lockdown_effect * model.inf],
+    name = :time_triggered_lockdown
+  )
 end
 
 end
